6fc9eda9488cb6b84dc396f7b377000e1357bb3e
charles <charles07job@gmail.com>
2025-08-05 12:12:50 -0300

    aumento de cobertura de testes unitários.

--- src/test/java/br/com/fiap/factory/RestaurantFactory.java
+++ src/test/java/br/com/fiap/factory/RestaurantFactory.java
@@ -1,6 +1,7 @@
 package br.com.fiap.factory;
 
 import br.com.fiap.dto.RestaurantDTO;
+import br.com.fiap.dto.RestaurantResponseDTO;
 import br.com.fiap.model.Restaurant;
 
 import java.time.LocalTime;
@@ -29,6 +30,16 @@ public class RestaurantFactory {
                 LocalTime.of(23, 0),
                 createUser(1L),
                 new ArrayList<>()
+        );
+    }
+
+    public static RestaurantResponseDTO getRestaurantResponseDTO() {
+        return new RestaurantResponseDTO(
+                "food",
+                "Av central",
+                "Pasta",
+                LocalTime.of(17, 0),
+                LocalTime.of(23, 0)
         );
     }
 }
